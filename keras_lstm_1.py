abd0d251ca5790abf4b1a352db45950581bc5b6a
Alec Yenter <alecyenter@csu.fullerton.edu>
2017-05-10 16:26:08 -0500

    adding plots

--- keras_lstm_1.py
+++ keras_lstm_1.py
@@ -1,5 +1,15 @@
+#TF_CPP_MIN_LOG_LEVEL=1 python keras_lstm_1.py --ssh 
+
 # LSTM and CNN for sequence classification in the IMDB dataset
+
+
+# -+-+-+-+-+-+-+- IMPORTS -+-+-+-+-+-+-+-
+
+import time
 import numpy
+import matplotlib
+import argparse
+import os
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
@@ -8,6 +18,43 @@ from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
+
+
+# -+-+-+-+-+-+-+- FUNCTIONS -+-+-+-+-+-+-+-
+
+def print_time(start, end):
+	hours, rem = divmod(end-start, 3600)
+	minutes, seconds = divmod(rem, 60)
+	print("{:0>2}h {:0>2}m {:05.2f}s ".format(int(hours),int(minutes),seconds))
+
+def plot_epochs(history):
+	plt.margins(.05,.1)
+	x = range(1,len(history['acc'])+1)
+	fig, ax1 = plt.subplots()
+	ax2 = ax1.twinx()
+
+	ax1.plot(x, history['acc'], 'g--')
+	ax1.plot(x, history['val_acc'], 'g-')
+	ax2.plot(x, history['loss'], 'r--')
+	ax2.plot(x, history['val_loss'], 'r-')
+
+	ax1.set_xlabel('Epochs')
+	ax1.set_ylabel('Accuracy', color='g')
+	ax2.set_ylabel('Loss', color='r')
+
+	plt.show()
+
+
+# -+-+-+-+-+-+-+- ARGUMENTS -+-+-+-+-+-+-+-
+
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+parser = argparse.ArgumentParser(description='Sentiment LSTM running through Keras on IMDb movie reviews')
+parser.add_argument('--ssh', dest="ssh", action="store_true", default=False, help="Change matplotlib back-end for ssh")
+inputs = parser.parse_args()
+
+
+# -+-+-+-+-+-+-+- DATA PREPROCESSING -+-+-+-+-+-+-+-
+
 # fix random seed for reproducibility
 numpy.random.seed(7)
 # load the dataset but only keep the top n words, zero the rest
@@ -17,6 +64,10 @@ top_words = 5000
 max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
+
+# -+-+-+-+-+-+-+- BUILDING MODEL -+-+-+-+-+-+-+-
+
 # create the model
 embedding_vecor_length = 32
 model = Sequential()
@@ -27,7 +78,26 @@ model.add(LSTM(100))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, epochs=3, batch_size=64)
+
+
+# -+-+-+-+-+-+-+- TRAINING MODEL -+-+-+-+-+-+-+-
+
+start_time = time.time()
+hist = model.fit(numpy.vstack((X_train,X_test)), numpy.hstack((y_train,y_test)), validation_split=0.5, epochs=3, batch_size=64)
+end_time = time.time()
+print_time(start_time, end_time)
+print("")
+
+
+# -+-+-+-+-+-+-+- EVALUATION AND PLOTTING -+-+-+-+-+-+-+-
+
 # Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1]*100))
+
+# import plt incase of 
+if inputs.ssh:
+	matplotlib.use('GTK')
+import matplotlib.pyplot as plt
+
+plot_epochs(hist.history)
