6c835e62c9620e47673e0ba4728aeb456164e6ca
Alec Yenter <alecyenter@csu.fullerton.edu>
2017-05-11 14:36:34 -0500

    fix branches

--- keras_lstm_1.py
+++ keras_lstm_1.py
@@ -13,7 +13,7 @@ import os
 import keras
 from keras.datasets import imdb
 from keras.models import Sequential
-from keras.layers import Dense, Merge, Input
+from keras.layers import Dense, Merge, Input, Reshape
 from keras.layers import LSTM
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
@@ -93,25 +93,22 @@ print("")
 print("BUILDING MODEL")
 embedding_vecor_length = 32
 
-input_layer = Input(shape=(500,))
+input_layer = Embedding(top_words, embedding_vecor_length, input_length=max_review_length)
 
 branch_3 = Sequential()
 branch_3.add(input_layer)
-branch_3.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
 branch_3.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 branch_3.add(MaxPooling1D(pool_size=2))
 branch_3.add(LSTM(100))
 
 branch_4 = Sequential()
 branch_4.add(input_layer)
-branch_4.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
 branch_4.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 branch_4.add(MaxPooling1D(pool_size=2))
 branch_4.add(LSTM(100))
 
 branch_5 = Sequential()
 branch_5.add(input_layer)
-branch_5.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
 branch_5.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
 branch_5.add(MaxPooling1D(pool_size=2))
 branch_5.add(LSTM(100))
