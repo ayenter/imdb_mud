64f9a5a6976ee15e9ce6a5e902b4f100653c5d38
Alec Yenter <alecyenter@csu.fullerton.edu>
2017-05-10 19:08:12 -0500

    added epochs arg

--- keras_lstm_1.py
+++ keras_lstm_1.py
@@ -69,6 +69,7 @@ def plot_epochs(history, batch_history):
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 parser = argparse.ArgumentParser(description='Sentiment LSTM running through Keras on IMDb movie reviews')
 parser.add_argument('--ssh', dest="ssh", action="store_true", default=False, help="Change matplotlib back-end for ssh")
+parser.add_argument('num_epochs', dest="num_epochs", action="store", default=3, help="Number of Epochs", type=int)
 inputs = parser.parse_args()
 
 
