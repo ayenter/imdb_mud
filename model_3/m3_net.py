a793569c023d8f28ccfefc088d6a9817d167cba3
Alec Yenter <alecyenter@csu.fullerton.edu>
2017-05-18 17:13:50 -0500

    model3

--- model_3/m3_net.py
+++ model_3/m3_net.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 
 # -+-+-+-+-+-+-+- GLOBAL VARIABLES -+-+-+-+-+-+-+-
 
-global_model = 2
+global_model = 3
 
 
 
@@ -158,6 +158,10 @@ embedding_vecor_length = 32
 
 input_layer = Embedding(top_words, embedding_vecor_length, input_length=max_review_length)
 
+branch_1 = Sequential()
+branch_1.add(input_layer)
+branch_1.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+
 branch_3 = Sequential()
 branch_3.add(input_layer)
 branch_3.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
@@ -177,7 +181,7 @@ branch_5.add(MaxPooling1D(pool_size=2))
 branch_5.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
 
 model = Sequential()
-model.add(Merge([branch_3,branch_4,branch_5], mode='concat'))
+model.add(Merge([branch_1,branch_3,branch_4,branch_5], mode='concat'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print("")
